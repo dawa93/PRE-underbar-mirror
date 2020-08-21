380271367c3062ff488de5f89fd86e8cc1f38d81
dawa93 <dawa93@naver.com>
2020-08-22 01:27:20 +0900

    fixed delay error

--- src/underbar.js
+++ src/underbar.js
@@ -339,9 +339,9 @@
   // someFunction('a', 'b') 은 500ms 이후에 호출됩니다.
   _.delay = function(func, wait, a, b) {
     if (arguments.length === 4) {
-      setTimeout(func(a, b), wait);
+      return setTimeout(func(a, b), wait);
     } else {
-      setTimeout(func, wait);
+      return setTimeout(func, wait);
     }
   };
 
@@ -359,26 +359,18 @@
     // 요소가 배열이 아니라면 새 배열에 추가.
     // 요소가 배열이면 재귀함수
     // base case 는 요소가 배열인지 아닌지
-    // if (result === undefined) {
-    //   result = [];
-    // }
+    if (result === undefined) {
+      result = [];
+    }
 
-    // if (!Array.isArray(nestedArray)) {
-    //   result.push(nestedArray);
-    // } else {
-    //   for (let element of nestedArray) {
-    //     _.flatten(element, result);
-    //   }
-    // }
-
-    // return result;
-
-    if (!result) result = [];
-    if (!Array.isArray(nestedArray)) result.push(nestedArray);
-    else
-      for (const n in nestedArray) {
-        _.flatten(nestedArray[n], result);
+    if (!Array.isArray(nestedArray)) {
+      result.push(nestedArray);
+    } else {
+      for (let element of nestedArray) {
+        _.flatten(element, result);
       }
+    }
+
     return result;
   };
   // 송찬영님 코드
